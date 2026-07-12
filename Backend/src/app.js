ee597675f3e9e8aabfd5a181a21da72e516307f1
Prashant Kumar <prashantkumar22205@gmail.com>
2026-07-12 13:53:36 +0530

    fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,8 +7,8 @@ app.use(express.static("public"))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    // origin:"https://interview-pilot-ai-peach.vercel.app",
-    origin:"http://localhost:5173",
+    origin:"https://interview-pilot-ai-peach.vercel.app",
+    // origin:"http://localhost:5173",
     credentials:true
 }))
 
